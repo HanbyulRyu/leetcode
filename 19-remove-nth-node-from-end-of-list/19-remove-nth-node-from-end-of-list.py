f834aca2becb6dbba1c2f028bfa23289f41f0f8c
HanbyulRyu <hbgod92@gmail.com>
2022-03-02 20:26:54 +0900

    Time: 50 ms (45.20%), Space: 13.9 MB (87.01%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -14,9 +14,15 @@ class Solution:
             length += 1
             lengthHead = lengthHead.next
         
+        # 지우고 싶은 위치
         position = length - n
               
-        if (length == 1 and n == 1) or position == 0:
+        # head 길이가 1이면서 첫번째 노드를 지우고 싶은 경우
+        if length == 1 and n == 1:
+            return head.next
+        
+        # 마지막 노드를 지우고 싶은 경우
+        if position == 0:
             return head.next
         
         # 원하는 위치의 앞까지 이동해야하기 때문에 > 1 까지만
