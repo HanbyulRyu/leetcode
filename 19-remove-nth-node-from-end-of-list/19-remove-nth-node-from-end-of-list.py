4498df19e118c96ea83ca7be0fc2a7e97792d3c8
HanbyulRyu <hbgod92@gmail.com>
2022-03-02 20:25:29 +0900

    Time: 48 ms (50.08%), Space: 13.8 MB (87.01%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -9,25 +9,22 @@ class Solution:
         temp = head
         length = 0
         
+        # head의 길이
         while lengthHead:
             length += 1
             lengthHead = lengthHead.next
         
         position = length - n
-        
+              
         if (length == 1 and n == 1) or position == 0:
             return head.next
         
-        
-        
+        # 원하는 위치의 앞까지 이동해야하기 때문에 > 1 까지만
         while position > 1:
             if temp.next:
                 temp = temp.next
                 position -= 1
-            else: 
-                return temp
-            
+
         temp.next = temp.next.next
-        
         return head
                     
