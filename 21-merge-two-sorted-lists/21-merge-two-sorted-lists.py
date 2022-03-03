0233e7a556fccf3965d29a6581aec5f32d531e91
HanbyulRyu <hbgod92@gmail.com>
2022-03-03 19:50:49 +0900

    Time: 53 ms (51.84%), Space: 14 MB (64.81%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -7,16 +7,15 @@ class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         head, start = None, None
         
-        
+        # 둘 다 빈 리스트일 경우 None, 하나만 빈 리스트 일경우 다른 하나를 return
         if not l1 and not l2:
             return head
-        
-        if not l1:
+        elif not l1:
             return l2
-        
-        if not l2:
+        elif not l2:
             return l1
         
+        # 둘 중 첫 값이 작은 값을 start 값에 셋팅
         if l1.val < l2.val:
             start = l1
             l1 = l1.next
@@ -25,7 +24,8 @@ class Solution:
             l2 = l2.next
 
         head = start
-        
+
+        # 작은 값을 넣어주도록 한다.
         while l1 and l2:
             next = None
             l1_v = l1.val
@@ -41,11 +41,11 @@ class Solution:
             start.next = next
             start = start.next
                 
+        # 남아있는 값을 뒤에 붙여준다.
         if l1:
             start.next = l1
         
         if l2:
             start.next = l2
             
-        print(head)
         return head
