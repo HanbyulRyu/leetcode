f07373619ec7cd374a1de787ab2e8878bafb4976
HanbyulRyu <hbgod92@gmail.com>
2022-04-26 00:48:11 +0900

    Time: 1443 ms (30.44%), Space: 15 MB (76.82%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output = []
-        
         for i in range(len(nums)):
             gap = target - nums[i]
             if gap in nums and i != nums.index(gap):
                 return [i, nums.index(gap)]
 
-        
         return output
                         
