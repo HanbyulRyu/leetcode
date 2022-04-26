a62177b54b3f813aaecb94da3dd555227d602313
HanbyulRyu <hbgod92@gmail.com>
2022-04-26 16:43:09 +0900

    Time: 77 ms (71.37%), Space: 13.8 MB (88.32%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,14 +5,14 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        tempNode = resultNode = ListNode()
-        resultArray = []
-        addNumber = 0
+        temp_node = result_node = ListNode()
+        add_n = 0
         
-        while l1 or l2 or addNumber:
+        while l1 or l2 or add_n:
             #print("l1_value", l1.val, "l2_value", l2.val, "addNumber", addNumber)
             l1_value = 0
             l2_value = 0
+            
             if l1:
                 l1_value = l1.val
                 l1 = l1.next
@@ -20,14 +20,14 @@ class Solution:
                 l2_value = l2.val
                 l2 = l2.next
         
-            sumNumber = l1_value + l2_value + addNumber
+            sum_n = l1_value + l2_value + add_n
            
-            result = sumNumber % 10
-            addNumber = sumNumber // 10
+            result = sum_n % 10
+            add_n = sum_n // 10
 
-            tempNode.next = ListNode(result)
-            tempNode = tempNode.next
+            temp_node.next = ListNode(result)
+            temp_node = temp_node.next
             #print(resultNode)
         
-        return resultNode.next
+        return result_node.next
                 
