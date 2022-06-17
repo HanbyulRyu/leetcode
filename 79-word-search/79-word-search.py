ce8f3beda634c3e8efcfd212685b2606cb16b7eb
HanbyulRyu <hbgod92@gmail.com>
2022-06-17 14:27:21 +0900

    Time: 4381 ms (91.20%), Space: 13.9 MB (50.25%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -3,37 +3,37 @@ class Solution:
         max_r = len(board)
         max_c = len(board[0])
 
-        def match(r, c, word):
+        def dfs(r, c, word):
             if not word:
                 return True
 
-            #이미 지나온 곳
+            # add1 : 이미 지나온 곳 표기
             temp = board[r][c]
             board[r][c] = "0"
             
             if 0 <= r+1 < max_r and 0 <= c < max_c and board[r+1][c] == word[0]:
-                if match(r+1, c, word[1:]):
+                if dfs(r+1, c, word[1:]):
                     return True
                 
             if 0 <= r-1 < max_r and 0 <= c < max_c and board[r-1][c] == word[0]:
-                if match(r-1, c, word[1:]):
+                if dfs(r-1, c, word[1:]):
                     return True
                 
             if 0 <= r < max_r and 0 <= c+1 < max_c and board[r][c+1] == word[0]:
-                if match(r, c+1, word[1:]):
+                if dfs(r, c+1, word[1:]):
                     return True
                 
             if 0 <= r < max_r and 0 <= c-1 < max_c and board[r][c-1] == word[0]:
-                if match(r, c-1, word[1:]):
+                if dfs(r, c-1, word[1:]):
                     return True
 
-            #이미 지나온 곳이지만 다시 처음부터 시작할 수도 있음....
+            # add2 : 이미 지나온 곳이지만 다시 처음부터 시작할 수도 있음 (롤백)
             board[r][c] = temp
                 
         for i in range(max_r):
             for j in range(max_c):
                 if board[i][j] == word[0]:
-                    if match(i, j, word[1:]):
+                    if dfs(i, j, word[1:]):
                         return True
 
         return False
