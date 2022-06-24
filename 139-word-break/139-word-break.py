09affe5fdd926f4a7b6006849637c2d284fe89c4
HanbyulRyu <hbgod92@gmail.com>
2022-06-24 15:48:25 +0900

    Time: 86 ms (11.12%), Space: 13.8 MB (94.66%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,4 +1,11 @@
 class Solution:
+    # 1 :: replace
+        # "cars", ["car","ca","rs"] 인 경우 false
+    # 2 :: replace 이지만 이중 for
+        # "cbca", ["bc","ca"] 인 경우 true 로 나옴
+    # 2-1 :: 처음부터 매칭 안되는 경우 false 로 리턴 처리
+        # "abcd", ["a","abc","b","cd"] 인 경우 false 로 나옴
+            
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         matched = [False for i in range(len(s)+1)]
         matched[0] = True
@@ -15,3 +22,5 @@ class Solution:
             return True
         
         return False
+    
+    
