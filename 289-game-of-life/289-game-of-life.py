00f0cf198988b58a6fc0e5e6b80b72154b9952c5
HanbyulRyu <hbgod92@gmail.com>
2022-07-01 16:45:26 +0900

    Time: 53 ms (44.20%), Space: 14 MB (10.28%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.py
@@ -0,0 +1,46 @@
+from copy import deepcopy
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        row = len(board[0])
+        col = len(board)
+        prev = deepcopy(board)
+        # print(prev)
+        
+        for i in range(col):
+            for j in range(row):
+                lives = 0
+                if i > 0:
+                    if j > 0 and prev[i-1][j-1]:
+                        lives += 1
+                    if prev[i-1][j]:
+                        lives += 1
+                    if j < row-1 and prev[i-1][j+1]:
+                        lives += 1
+                if j > 0 and prev[i][j-1]:
+                    lives += 1
+                if j < row-1 and prev[i][j+1]:
+                    lives += 1
+                if i < col-1:
+                    if j > 0 and prev[i+1][j-1]:
+                        lives += 1
+                    if prev[i+1][j]:
+                        lives += 1
+                    if j < row-1 and prev[i+1][j+1]:
+                        lives += 1
+                # print(i, j, ":", lives)
+                
+                if lives < 2 or lives > 3:
+                    board[i][j] = 0
+                if lives == 3:
+                    board[i][j] = 1
+                    print(i, j, ":", board[i][j])
+                
+        # print(board)
+        
+        
+        
+        
