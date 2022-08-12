4f32225e0cb1ab5cdb877acf9644032e4c050a0f
HanbyulRyu <hbgod92@gmail.com>
2022-08-12 15:57:48 +0900

    Time: 51 ms (46.27%), Space: 13.9 MB (32.95%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,36 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[0 for i in range(n)] for j in range(m)]
+        
+        for i in range(len(grid)) :
+            for j in range(len(grid[i])):
+                if i == 0 or j == 0:
+                    grid[i][j] = 1
+                   
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[i])):
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+                
+        return grid[m-1][n-1]
+        
+#         global result
+#         result = 0
+        
+#         def paint(i, j):
+#             global result
+#             if i == m-1 or j == n-1:
+#                 result += 1
+#                 return
+                
+#             if i < m-1:
+#                 paint(i+1, j)
+
+#             if j < n-1:
+#                 paint(i, j+1)
+            
+#         paint(0, 0)
+        
+#         return result
+                
+            
+        
